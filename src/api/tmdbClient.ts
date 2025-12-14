8aae77b49148d3efe1bfbd9ba9503d2384fa4259
202413059-cloud <202413059@jbnu.ac.kr>
2025-12-15 03:32:52 +0900

    Update tmdbClient.ts

--- src/api/tmdbClient.ts
+++ src/api/tmdbClient.ts
@@ -1,5 +1,3 @@
-console.log('ENV KEY:', import.meta.env.VITE_TMDB_API_KEY)
-
 import axios from 'axios'
 
 const baseURL = import.meta.env.VITE_TMDB_BASE_URL
@@ -10,14 +8,17 @@ export const tmdb = axios.create({
 })
 
 tmdb.interceptors.request.use((config) => {
-  // 과제 요구: 로그인하면 localStorage의 TMDb-Key 사용 가능
-  const stored = localStorage.getItem('TMDb-Key')
-  const fallback = import.meta.env.VITE_TMDB_API_KEY
-  const apiKey = stored || fallback
+  // ✅ TMDB v4 Read Access Token
+  const token =
+    localStorage.getItem('TMDb-Key') || import.meta.env.VITE_TMDB_API_KEY
+
+  config.headers = {
+    ...(config.headers || {}),
+    Authorization: `Bearer ${token}`,
+  }
 
   config.params = {
     ...(config.params || {}),
-    api_key: apiKey,
     language: 'ko-KR',
   }
 
