c4a627aac556278443296d718844b15a6cf78a0e
202413059-cloud <202413059@jbnu.ac.kr>
2025-12-15 01:47:16 +0900

    Set up Vue Router with multiple routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,1 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/pages/Home.vue'
+import SignIn from '@/pages/SignIn.vue'
+import Popular from '@/pages/Popular.vue'
+import Search from '@/pages/Search.vue'
+import Wishlist from '@/pages/Wishlist.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/signin', component: SignIn },
+    { path: '/', component: Home },
+    { path: '/popular', component: Popular },
+    { path: '/search', component: Search },
+    { path: '/wishlist', component: Wishlist },
+  ],
+})
+
+export default router
 
