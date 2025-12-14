7dcabc058c93fe55edb895ef45cc9142beec6661
202413059-cloud <202413059@jbnu.ac.kr>
2025-12-15 01:49:35 +0900

    Add authentication check before route navigation
    Implement route guard to redirect unauthenticated users.

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,8 @@ import SignIn from '@/pages/SignIn.vue'
 import Popular from '@/pages/Popular.vue'
 import Search from '@/pages/Search.vue'
 import Wishlist from '@/pages/Wishlist.vue'
+import { isLoggedIn } from '@/utils/auth'
+
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,6 +16,18 @@ const router = createRouter({
     { path: '/search', component: Search },
     { path: '/wishlist', component: Wishlist },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const loggedIn = isLoggedIn()
+
+  if (!loggedIn && to.path !== '/signin') {
+    next('/signin')
+  } else if (loggedIn && to.path === '/signin') {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
