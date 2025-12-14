7d3922634f764385a7cbe5deba452cacc59430af
202413059-cloud <202413059@jbnu.ac.kr>
2025-12-15 01:43:16 +0900

    Update and rename HelloWorld.vue to Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class="header">
+    <h1 class="logo">NETFLIX DEMO</h1>
+
+    <nav>
+      <a href="/">Home</a>
+      <a href="/popular">Popular</a>
+      <a href="/search">Search</a>
+      <a href="/wishlist">Wishlist</a>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+  background: black;
+  color: white;
+}
+
+nav a {
+  margin-left: 12px;
+  color: white;
+  text-decoration: none;
+}
+</style>
