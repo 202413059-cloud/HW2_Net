2bc2237ce97b31da0270be2217347c05c3f3b24a
202413059-cloud <202413059@jbnu.ac.kr>
2025-12-15 13:10:15 +0900

    Update url.ts

--- src/api/url.ts
+++ src/api/url.ts
@@ -3,4 +3,6 @@ export const endpoints = {
   nowPlaying: (page = 1) => ({ url: '/movie/now_playing', params: { page } }),
   topRated: (page = 1) => ({ url: '/movie/top_rated', params: { page } }),
   upcoming: (page = 1) => ({ url: '/movie/upcoming', params: { page } }),
+  genres: () => ({ url: '/genre/movie/list', params: {} }),
+  discover: (params: Record<string, any>) => ({ url: '/discover/movie', params }),
 }
